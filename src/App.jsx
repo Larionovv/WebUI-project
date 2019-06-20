29466d6732f5f9f03e0f9ab7157e579c7fcaed33
Larionovv <larionovv@i.ua>
2019-06-20 09:13:09 +0300

    created react app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  render() {
+    return (
+    < div >
+      Fun Page
+    < /div>
+  )
+    ;
+  }
+};
+
